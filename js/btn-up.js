30e0cae8e226907723f7a20f3e97fc69baea5c8c
Edgar Segura <edgarseguraflores@gmail.com>
2021-05-22 16:27:34 -0500

    Se agrego boton hacia arriaba

---
+++ js/btn-up.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function upPage(){
+    let btnUp = document.querySelector('#buttonUp');
+    let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    
+}
+
+window.onload = upPage();
